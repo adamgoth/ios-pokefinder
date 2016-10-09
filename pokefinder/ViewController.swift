44ac2dc586faa1ced6229bf76143fd96ecd13f5b
Adam Goth <adamgoth@gmail.com>
2016-10-09 16:15:19 -0600

    Conigurefigures Firebase and GeoFire

--- pokefinder/ViewController.swift
+++ pokefinder/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var geofire: GeoFire!
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ pokefinder/pokefinder-bridging-header.h
@@ -0,0 +1,9 @@
+//
+//  pokefinder-bridging-header.h
+//  pokefinder
+//
+//  Created by Adam Goth on 10/9/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+#import "GeoFire.h"
